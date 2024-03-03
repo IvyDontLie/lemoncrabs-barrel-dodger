39b368a79c2268ac963005d4e9ef0083c2e29b44
J1GGS <156314387+IvyDontLie@users.noreply.github.com>
2024-03-03 15:11:23 -0500

    added moving

--- main.ts
+++ main.ts
@@ -12,7 +12,9 @@ tiles.setTilemap(tilemap`level2`)
 mySprite = sprites.create(assets.image`lemoncrab`, SpriteKind.Player)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 5))
 mySprite.ay = 500
+controller.moveSprite(mySprite, 100, 0)
 music.play(music.stringPlayable("G F G A - F E D ", 120), music.PlaybackMode.UntilDone)
+controller.moveSprite(mySprite, 100, 0)
 game.onUpdateInterval(2000, function () {
     projectile = sprites.createProjectileFromSide(assets.image`barrel`, randint(-100, -80), 0)
     tiles.placeOnTile(projectile, tiles.getTileLocation(9, 5))
